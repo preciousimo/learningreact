06417e2fb6a1a104cabdb9596e8a498c3074c73a
preciousimo <preciousimoniakemu@gmail.com>
2022-02-10 19:28:41 +0100

    Quiz!

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const navbar = (
-    <nav>
-        <h1>Prochi Limited</h1>
+const page = (
+    <div>
+        <img width="40px" src="https://mpng.subpng.com/20180604/pol/kisspng-react-javascript-angularjs-ionic-atom-5b154be6709500.6532453515281223424611.jpg" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousand of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
-ReactDOM.render(navbar, document.getElementById("root"))
+ReactDOM.render(page, document.getElementById("root"))
