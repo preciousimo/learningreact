d167f50bf1a3ff43df46e2d5b2d9d9beb24c7a24
preciousimo <preciousimoniakemu@gmail.com>
2022-02-10 11:22:30 +0100

    First react code!

---
+++ index.js
@@ -0,0 +1,16 @@
+// function MainContent() {
+//     return(
+//       <h2>I'm learning react!</h2>
+//     )
+//   }
+
+// ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+// ReactDOM.render(<MainContent/>, document.getElementById("root"))
+
+function MainContent() {
+    return(
+        <h1>I'm learning react!</h1>
+    )
+}
+
+ReactDOM.render(<MainContent/>, document.getElementById("root"))
