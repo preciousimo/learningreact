04be8d7fb5ede86a677885eacf97eb1f9c903d7c
preciousimo <preciousimoniakemu@gmail.com>
2022-02-10 19:35:09 +0100

    Another test

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const page = (
-    <div>
-        <img width="40px" src="https://mpng.subpng.com/20180604/pol/kisspng-react-javascript-angularjs-ionic-atom-5b154be6709500.6532453515281223424611.jpg" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousand of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+function TemporaryName() {
+    return(
+        <div>
+            {/* <img src="./react-logo.png" width="40px" /> */}
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(<TemporaryName/>, document.getElementById("root"))
