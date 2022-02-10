8afa73f9affe44915b41897b7d0d5b2c6abd1dec
preciousimo <preciousimoniakemu@gmail.com>
2022-02-10 19:37:37 +0100

    Challenge: part 1

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function TemporaryName() {
+// Part 1: Create a page of your own using a custom Page component
+
+// It should return an ordered list with the reasons why you're
+// excited to be learning React :)
+
+// Render your list to the page
+
+
+function MyPage() {
     return(
         <div>
-            {/* <img src="./react-logo.png" width="40px" /> */}
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+            <ol>
+            <li>I love it</li>
+            <li>I want to be a fullstack dev</li>
+            <li>I need money</li>
+            </ol>
         </div>
     )
 }
 
-ReactDOM.render(<TemporaryName/>, document.getElementById("root"))
+ReactDOM.render(<MyPage/>, document.getElementById("root"))
