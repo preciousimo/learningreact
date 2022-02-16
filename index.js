515c58a7102095d85fc53cb4a0e9dc36517e0ebb
preciousimo <preciousimoniakemu@gmail.com>
2022-02-16 10:33:00 +0100

    Quiz!

--- index.js
+++ index.js
@@ -5,17 +5,26 @@
 
 // Render your list to the page
 
-
-function MyPage() {
-    return(
+function Page() {
+    return (
         <div>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
             <ol>
-            <li>I love it</li>
-            <li>I want to be a fullstack dev</li>
-            <li>I need money</li>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
             </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
         </div>
     )
 }
 
-ReactDOM.render(<MyPage/>, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
