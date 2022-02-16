2bef2699f116c3f4fd21f12e50a1a56b294c2967
preciousimo <preciousimoniakemu@gmail.com>
2022-02-16 12:29:13 +0100

    Parent/Child Component

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-// Part 1: Create a page of your own using a custom Page component
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="react.jpg" width="250px"/>
+            </nav>
+        </header>
+    )
+}   
 
-// It should return an ordered list with the reasons why you're
-// excited to be learning React :)
+function Footer() {
+    return (
+        <footer>
+            <small>© 2022 Prochi. All rights reserved.</small>
+        </footer>
+    )
+}
 
-// Render your list to the page
+function MainContent() {
+    return (
+        <div>
+        <h1>Reasons i'm excited to learn React</h1>
+        <ol>
+            <li>It's a popular library</li>
+            <li>I'm more likely to get a job as a developer</li>
+        </ol>
+        </div>
+    )
+}
 
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
+            <Header />
+
+            <MainContent />
+
+            <Footer />
         </div>
     )
 }
