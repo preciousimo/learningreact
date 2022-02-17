5c05b57a1cabc978e8fd251758746fd5b349f325
preciousimo <preciousimoniakemu@gmail.com>
2022-02-17 12:06:11 +0100

    Styling with classes

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+/**
+Challenge: 
+
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+*/
+
 function Header() {
     return (
         <header>
-            <nav>
-                <img src="react.jpg" width="250px"/>
+            <nav className="nav">
+                <img className="nav-logo" src="react.jpg" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     )
-}   
+}
 
 function Footer() {
     return (
         <footer>
-            <small>© 2022 Prochi. All rights reserved.</small>
+            <small>© 2021 Prochi development. All rights reserved.</small>
         </footer>
     )
 }
@@ -19,11 +31,13 @@ function Footer() {
 function MainContent() {
     return (
         <div>
-        <h1>Reasons i'm excited to learn React</h1>
-        <ol>
-            <li>It's a popular library</li>
-            <li>I'm more likely to get a job as a developer</li>
-        </ol>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
         </div>
     )
 }
@@ -32,9 +46,7 @@ function Page() {
     return (
         <div>
             <Header />
-
             <MainContent />
-
             <Footer />
         </div>
     )
