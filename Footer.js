a4e48a1e0db91ca2c8f07019ccc1a1839821abd6
preciousimo <preciousimoniakemu@gmail.com>
2022-02-17 13:45:45 +0100

    Organize components

---
+++ Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Prochi development. All rights reserved.</small>
+        </footer>
+    )
+}
--- index.js
+++ index.js
@@ -1,48 +1,8 @@
-/**
-Challenge: 
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-*/
-
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-                <img className="nav-logo" src="react.jpg" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2021 Prochi development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
-    )
-}
-
-function Page() {
+function App() {
     return (
         <div>
             <Header />
@@ -52,4 +12,4 @@ function Page() {
     )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
